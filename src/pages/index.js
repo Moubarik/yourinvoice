a1f3cd7b9e5fb9fb673dda7d8bf3d9389475adce
Moubarik <Moubarik20@gmail.com>
2022-06-25 23:04:36 +0100

    push again and again

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Home from "./Home"
-
 import Header from "./components/Header"
-
 import ScrollToTop from "./components/ScrollToTop"
 import ContactForm from "./components/ContactForm"
 import Footer from "./components/Footer"
@@ -9,12 +7,9 @@ import Error from "./Error"
 
 export {
   Home,
-  
   Header,
-
   ScrollToTop,
   ContactForm,
   Footer,
   Error,
-
 }
